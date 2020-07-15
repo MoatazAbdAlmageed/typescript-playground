fe949fcfecace92a97b45f9418461225b2342257
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-15 14:53:48 +0200

    creader html

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,34 @@ class Customer {
     this.position = position;
     this.company = company;
   }
+
   hello(): string {
     return `hello my name  is ${this.name}
     I have ${this.age} years old
     I'm a  ${this.position} ad ${this.company}
     `;
   }
+
+  setAge(age: number): number {
+    this.age = age;
+    return this.age;
+  }
+  setCompany(company: string): string {
+    this.company = company;
+    return this.company;
+  }
+  setPosition(position: string): string {
+    this.position = position;
+    return this.position;
+  }
 }
-let customer = new Customer("maoataz", 30, "web developer", "Wuilt");
+
+let customer = new Customer("maoataz", 29, "PHP web developer", "camelCase");
 console.log(customer);
-console.log(customer.hello());
+
+// year ago (1/1/2020)
+customer.setAge(30);
+customer.setCompany("wuilt");
+customer.setPosition("MERN developer");
+
+console.log(customer);
